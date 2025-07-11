331eb816ed7cd8b47a3ac2b252bf29b0642e53de
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-10 20:52:50 -0400

    initial commit

---
+++ Playground/Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Nikhil Polepalli on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Playground/PlaygroundTests/PlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundTests.swift
+//  PlaygroundTests
+//
+//  Created by Nikhil Polepalli on 7/10/25.
+//
+
+import Testing
+@testable import Playground
+
+struct PlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

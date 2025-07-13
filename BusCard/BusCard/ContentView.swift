dbe519cb21cd0b6064fdf98a966f42a648a853c3
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-12 21:32:27 -0400

    add in bus card

--- BusCard/BusCard/ContentView.swift
+++ BusCard/BusCard/ContentView.swift
@@ -11,11 +11,29 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             Color(red: 0.09, green: 0.63, blue: 0.52).edgesIgnoringSafeArea(.all)
-            Text("Nikhil Polepalli")
+            VStack {
+                Image("NikhilP").resizable().aspectRatio(contentMode: .fit).frame(width: 200.0, height: 300.0).clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Nikhil Polepalli").font(.system(size: 40)).bold().foregroundColor(.white)
+                Text("iOS Developer").foregroundColor(.white).font(.system(size: 25))
+                Divider()
+                InfoView(text: "502-424-7440", imageName: "phone.fill")
+
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25).fill(Color.white).frame(height: 50).overlay(HStack{
+            Image(systemName: imageName).foregroundColor(.green)
+            Text(text)}).padding(.all)
+    }
 }

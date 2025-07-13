df9126b1ab0f01fbf60172c693d77782ec6710ea
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-12 21:51:56 -0400

    add in functionality

--- Dicee/Dicee/ContentView.swift
+++ Dicee/Dicee/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDice = 1
+    @State var rightDice = 1
+    
     var body: some View {
         ZStack{
             Image("background").resizable().edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDice)
+                    DiceView(n: rightDice)
+                }.padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    leftDice = Int.random(in: 1...6)
+                    rightDice = Int.random(in: 1...6)
+                }){
+                    Text("Roll").font(.system(size: 50)).fontWeight(.bold).foregroundColor(.white).padding(.horizontal)
+                }.background(Color.red).frame(height: 75)
+            }
         }
 
     }
@@ -18,4 +37,11 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)").resizable().aspectRatio(1, contentMode: .fit).padding()
+    }
 }

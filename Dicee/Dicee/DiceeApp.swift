598e8a21dd5d94d3d859b112e873e77441639603
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-12 21:38:22 -0400

    dicce init

---
+++ Dicee/Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Nikhil Polepalli on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dicee/DiceeTests/DiceeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeTests.swift
+//  DiceeTests
+//
+//  Created by Nikhil Polepalli on 7/12/25.
+//
+
+import Testing
+@testable import Dicee
+
+struct DiceeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
